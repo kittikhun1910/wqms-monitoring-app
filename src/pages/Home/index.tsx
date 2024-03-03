fcec6f096df2cf2749e8894041839c8e5dc9dd3f
Kittikhun <tumgamemar@gmail.com>
2024-03-03 23:53:22 +0700

    add Homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import styles from "./index.module.scss";
 import { Navbar } from "../../components";
+import ShimpPondPNG from "/src/images/shimp-pond.jpg";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
     <>
       <Navbar />
       <div className={styles.container}>
-        <div className={styles.content}>hi Home</div>
+        <div className={styles.contentLeft}>
+          <img src={ShimpPondPNG} alt="ShimpPondPNG" />
+        </div>
+        <div className={styles.contentRight}>
+          <div className={styles.header}>
+            <h1>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque,
+              et.
+            </h1>
+          </div>
+          <Link to={"/Login"} style={{ textDecoration: "none" }}>
+            <div className={styles.buttonGetStart}>Get Started</div>
+          </Link>
+        </div>
       </div>
     </>
   );
