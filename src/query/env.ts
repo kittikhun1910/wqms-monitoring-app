d28608054f7f517f29b5743414e67c985c9a481a
Kittikhun Klinnarai <71427085+kittikhun1910@users.noreply.github.com>
2024-03-18 12:55:48 +0700

    Update env.ts

--- src/query/env.ts
+++ src/query/env.ts
@@ -1,8 +1,8 @@
-const INFLUX_URL: string = "http://54.255.69.30:8086";
+const INFLUX_URL: string = "http://localhost:8086";
 const INFLUX_TOKEN: string =
-  "QsTrDIpewnDxamE8o3SXpFxq-wngKBKtQ5vV16DLKe13wA45vtPBoqxedd6CAIVwS7G9EcIcKFJQ81Cleq0JkA==";
-const INFLUX_ORG: string = "wqms_iot";
-const INFLUX_BUCKET: string = "wqms_data";
+  "your-token";
+const INFLUX_ORG: string = "my-org";
+const INFLUX_BUCKET: string = "my-data";
 
 export { 
   INFLUX_URL, 
